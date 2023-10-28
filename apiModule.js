8c18b1f81862a6d507a5d630ab0c2c38c020de68
trevo-community <147546698+trevo-community@users.noreply.github.com>
2023-10-28 20:03:28 -0300

    Update apiModule.js

---
+++ apiModule.js
@@ -0,0 +1,89 @@
+const axios = require('axios');
+
+const baseURL = 'dark-lime-snapper-hat.cyclic.app';
+
+async function fetchData(route, queryParams) {
+  const url = ${baseURL}${route}?${queryParams};
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+}
+
+module.exports = {
+  getAnimeInfo: async function(animeType, username, key) {
+    return fetchData(/nime/${animeType}, username=${username}&key=${key});
+  },
+  
+  getNSFWInfo: async function(nsfwType, username, key) {
+    return fetchData(/nsfw/${nsfwType}, username=${username}&key=${key});
+  },
+  
+  getFoto18: async function(username, key) {
+    return fetchData(/18/foto_18, username=${username}&key=${key});
+  },
+  
+  getVideo18: async function(username, key) {
+    return fetchData(/18/video, username=${username}&key=${key});
+  },
+  
+  getYTVideoDownloadLink: async function(videoUrl, username, key) {
+    return fetchData(
+      '/anikit/ytmp4',
+      username=${username}&key=${key}&videoUrl=${encodeURIComponent(videoUrl)}
+    );
+  },
+  
+  getYTAudioDownloadLink: async function(videoUrl, username, key) {
+    return fetchData(
+      '/anikit/ytmp3',
+      username=${username}&key=${key}&videoUrl=${encodeURIComponent(videoUrl)}
+    );
+  },
+
+  playMP4: async function(query, username, key) {
+    return fetchData(
+      '/anikit/playmp4',
+      username=${username}&key=${key}&query=${encodeURIComponent(query)}
+    );
+  },
+  
+  playMP3: async function(query, username, key) {
+    return fetchData(
+      '/anikit/playmp3',
+      username=${username}&key=${key}&query=${encodeURIComponent(query)}
+    );
+  },
+  
+  getRecentEpisodes: async function(username, key) {
+    return fetchData(
+      '/recent-episodes',
+      &username=${username}&key=${key}
+    );
+  },
+
+  getEpisodeInfo: async function(animeId, username, key) {
+    return fetchData(/epis/${animeId}, animeId=${animeId}&username=${username}&key=${key});
+  },
+
+  getAnimeInfoById: async function(animeId, username, key) {
+    return fetchData(/info/${animeId}, animeId=${animeId}&username=${username}&key=${key});
+  },
+  
+  getWatchEpisodeSource: async function(episodeId, username, key) {
+    return fetchData(
+      /watch/${episodeId},
+      episodeId=${episodeId}&username=${username}&key=${key}
+    );
+  },
+  
+  getTikTokVideoDownloadLink: async function(videoUrl, username, key) {
+    return fetchData(
+      '/anikit/tiktok',
+      videoUrl=${encodeURIComponent(videoUrl)}&username=${username}&key=${key}
+    );
+  }
+};
